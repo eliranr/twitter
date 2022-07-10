c2dbcfb6c5369b3d3c902a412a3829c01e3d61d9
eliranr <eliranr497@gmail.com>
2022-07-10 18:29:35 +0300

    fix errors for deploy

--- components/Widgets.js
+++ components/Widgets.js
@@ -16,7 +16,7 @@ export default function Widgets({newResults, randomUsersResults}) {
             </div>
         </div>
         <div className="text-gray-700 space-y-3 bg-gray-100 rounded-xl pt-2 w-[90%] xl:w-[75%]">
-            <h4 className="font-bold text-xl px-4">What't Happening</h4>
+            <h4 className="font-bold text-xl px-4">What&apos;t Happening</h4>
             <AnimatePresence>
                 {newResults.slice(0, articleNum).map((article) => (
                     <motion.div 
